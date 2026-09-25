40cd723c177775cdb5a39c5990d0ce147499d2b0
Hà Minh Phúc <hmuhmu123321@gmail.com>
2026-09-25 23:10:14 +0700

    TvcLesson09Annotation

---
+++ TvcLesson09Annotation/Models/DataViewModels/TvcMemberRegister.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TvcLesson09Annotation.Models.DataViewModels
+{
+    /// <summary>
+    /// Data Annotation - Validation
+    /// </summary>
+    public class TvcMemberRegister
+    {
+        public int TvcMemberId { get; set; }
+
+        [DisplayName("Tên đăng nhập")]
+        [Required(ErrorMessage = "Tên đăng nhập không để trống")]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Tên đăng nhập có độ dài trong khoảng 2 - 20 ký tự")]
+        public string TvcUserName { get; set; }
+
+        [DisplayName("Mật khẩu")]
+        [Required(ErrorMessage = "Mật khẩu không được để trống")]
+        [DataType(DataType.Password)]
+        public string TvcPassword { get; set; }
+
+        public string TvcEmail { get; set; }
+
+        public string TvcPhoneNumber { get; set; }
+
+        public string TvcFullName { get; set; }
+
+        public DateTime TvcBirthday { get; set; }
+    }
+}
---
+++ TvcLesson10EFDbFirst/Models/TvcMember.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TvcLesson10EFDbFirst.Models;
+
+public partial class TvcMember
+{
+    public long Id { get; set; }
+
+    public string? TvcUsername { get; set; }
+
+    public string? TvcPassword { get; set; }
+
+    public string? TvcFullName { get; set; }
+
+    public string? TvcEmail { get; set; }
+
+    public string? TvcPhone { get; set; }
+
+    public bool? TvcStatus { get; set; }
+}
